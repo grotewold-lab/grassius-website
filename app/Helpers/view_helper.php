de9ba35e497d5a74eb59efd8f0b082b87533ed5c
tessmero <grotewoldlab@gmail.com>
2023-01-06 08:16:18 -0500

    cleanup css and copy/expand buttons

--- app/Helpers/view_helper.php
+++ app/Helpers/view_helper.php
@@ -225,7 +225,7 @@ function get_copy_button($string_to_copy)
     
     return "
         <div class='tooltip'>
-            <image class='copy-button' src='/images/copy.png' onclick='myFunction$uid()' onmouseout='outFunc$uid()' >
+            <image class='copy-button' src='/images/clipboard.svg' onclick='myFunction$uid()' onmouseout='outFunc$uid()' >
                 <span class='tooltiptext' id='myTooltip$uid'>Copy to clipboard</span>
             </image>
         </div>
@@ -264,7 +264,7 @@ function get_expand_button( $target_class )
     
     return "
         <div class='tooltip'>
-            <image class='copy-button'  id='myTooltipImg$uid' src='/images/expand.png' onclick='myFunction$uid()'>
+            <image class='expand-button'  id='myTooltipImg$uid' src='/images/arrows-expand.svg' onclick='myFunction$uid()'>
                 <span class='tooltiptext' id='myTooltip$uid' data-state='collapsed'>Expand</span>
             </image>
         </div>
@@ -282,12 +282,12 @@ function get_expand_button( $target_class )
             $('.$target_class.long').show();
             tooltip.attr('data-state','expanded');
             tooltip.html('Collapse');
-            image.attr('src','/images/collapse.png')
+            image.attr('src','/images/arrows-collapse.svg')
           } else {
             $('.$target_class.short').show();
             tooltip.attr('data-state','collapsed');
             tooltip.html('Expand');
-            image.attr('src','/images/expand.png')
+            image.attr('src','/images/arrows-expand.svg')
           }
         }
         </script>
