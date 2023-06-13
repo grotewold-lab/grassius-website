f3b7abd1665b85d233ad1610b7cb6458ed8c9c18
Oliver Tessmer <oliver.tessmer@gmail.com>
2023-06-13 11:38:47 -0400

    (published jun13-2023) updated urls

--- app/Views/common/layout.php
+++ app/Views/common/layout.php
@@ -17,9 +17,9 @@
     
     <script src="/js/typeahead.min.js"></script>
     
-    <link rel="stylesheet" href="/css/gramene_org.css" type="text/css">
+    <link rel="stylesheet" href="/css/gramene_org.css?v=1" type="text/css">
     
-    <link rel="stylesheet" href="/css/gdc_cancer_gov.css" type="text/css">
+    <link rel="stylesheet" href="/css/gdc_cancer_gov.css?v=1" type="text/css">
     <script type="text/javascript" src="/js/wfui.js"></script>
  
     
@@ -29,7 +29,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js" integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css" integrity="sha512-ELV+xyi8IhEApPS/pSj66+Jiw+sOT1Mqkzlh8ExXihe4zfqbWkxPRi8wptXIO9g73FSlhmquFlUOuMSoXz5IRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
-    <link rel="stylesheet" href="/css/master.css" type="text/css">
+    <link rel="stylesheet" href="/css/master.css?v=1" type="text/css">
     
     <!--   <script src="js/sorttable.js"></script> -->
     <title><?php echo $title ?></title>
