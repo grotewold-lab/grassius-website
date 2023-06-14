52d41e6409cb40300dfd4f99398d21a5421dacfe
tessmero <grotewoldlab@gmail.com>
2023-06-14 12:46:52 -0400

    removed incorrect link from hitcoutn map on homepage

--- app/Views/common/hitcount_map.php
+++ app/Views/common/hitcount_map.php
@@ -1,5 +1,2 @@
 
-
-<div align="center"><a href="https://www.amazingcounters.com">AmazingCounters.com</a></div>
-
  <div id="counter">  <script type="text/javascript" id="clustrmaps" src="//cdn.clustrmaps.com/map_v2.js?d=iFeqerATlv0erLz8E_UJHNdsFkXIcgmSnrxV-9jOFMs&cl=ffffff&w=a"></script> </div>
